9b762df66ab8c513c92257739fcc78ad3ffb9a44
d-andres <daniel.andres.lopez91@gmail.com>
2020-09-06 15:48:29 -0500

    Removed darius project

--- frontend-darius/src/About.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export const About = () => (
-    <div>
-        <h1>So you don't have to worry much, okay?</h1>
-    </div>
-)
--- frontend-darius/src/Home.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export const Home = () => (
-    <div>
-        <h1>Hi! I'm working properly.</h1>
-    </div>
-)
