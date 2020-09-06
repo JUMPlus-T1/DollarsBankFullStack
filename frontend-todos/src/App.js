366e8ce963312bc4534e3b34125a6e30a54e39cf
d-andres <daniel.andres.lopez91@gmail.com>
2020-09-06 12:38:10 -0500

    Added example frontend

---
+++ frontend-todos/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TodoComponent from './components/TodoComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoComponent />
+    </div>
+  );
+}
+
+export default App;
