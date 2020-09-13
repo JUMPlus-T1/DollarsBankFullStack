4787562b6951512db2e2463a0aa3ede4c2fd78b5
d-andres <daniel.andres.lopez91@gmail.com>
2020-09-13 14:14:27 -0500

    Update App.js
    - added home, deposit, withdraw, transfer, information components and routes

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import Home from './components/user-menu/Home';
+import Deposit from './components/user-menu/Deposit';
+import Withdraw from './components/user-menu/Withdraw';
+import Transfer from './components/user-menu/Transfer';
+import Information from './components/user-menu/Information';
 import About from './components/About';
 import Contact from './components/Contact';
 import Login from './components/Login';
@@ -22,8 +26,12 @@ function App() {
           <Route path="/contact" component={Contact}/>
           <Route path="/login" component={Login}/>
           <Route path="/register" component={Register}/>
-          <Route path="/home" component={Home}/>
           <Route path="/logout" component={Logout}/>
+          <Route path="/home" component={Home}/>
+          <Route path="/deposit" component={Deposit}/>
+          <Route path="/withdraw" component={Withdraw}/>
+          <Route path="/transfer" component={Transfer}/>
+          <Route path="/information" component={Information}/>
           <Route path="/error" component={Error}/>
         </Switch>
       </Router>
