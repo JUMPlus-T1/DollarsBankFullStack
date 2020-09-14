2d8ad68f7ccbc3d7f8958b60ab6e772e820f55e1
d-andres <daniel.andres.lopez91@gmail.com>
2020-09-13 22:30:32 -0500

    Update backend
    added necessary code to controller to allow for deposit, withdraw, transfer transactions

--- backend-spring/src/main/java/com/cognixia/dollarsbank/repository/AccountRepository.java
+++ backend-spring/src/main/java/com/cognixia/dollarsbank/repository/AccountRepository.java
@@ -1,8 +1,11 @@
 package com.cognixia.dollarsbank.repository;
 
+import javax.transaction.Transactional;
+
 import com.cognixia.dollarsbank.model.Account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +14,18 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 	
 	@Query("SELECT a FROM Account a WHERE a.email=?1 AND a.password=?2")
 	Account findByEmailPassword(String email, String password);
+
+	@Query("SELECT a FROM Account a WHERE a.id=?1")
+	Account findAccountById(Long id);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE Account a SET a.balance=?2 WHERE a.id=?1")
+	void updateBalanceById(Long id, double balance);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE Account a SET a.history=?2 WHERE a.id=?1")
+	void updateHistoryById(Long id, String history);
 
 }
