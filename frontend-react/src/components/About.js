dee3b2a363f0477378b2571525e7fb57687e811f
d-andres <daniel.andres.lopez91@gmail.com>
2020-09-14 23:46:18 -0500

    Added coins image for about page

--- frontend-react/src/components/About.js
+++ frontend-react/src/components/About.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import ImageStrip from './ImageStrip';
+import styled from 'styled-components';
+import coins from '../images/coins.jpg';
+
+const Styles = styled.div`
+    .about {
+        background: url(${coins}) no-repeat fixed bottom;
+        background-size: cover;
+        color: #efefef;
+        height: 280px;
+        position: relative;
+        z-index: -2;
+    }
+`;
 
 class About extends React.Component {
     
@@ -14,8 +26,17 @@ class About extends React.Component {
     render() {
         return (
             <div>
-                <ImageStrip />
-                <h1>So you don't have to worry much, okay?</h1>
+                <Styles>
+                <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light" className="about">
+                    <div class="col-md-5 p-lg-5 mx-auto my-5">
+                        <h1 class="display-4 font-weight-normal">About Us</h1>
+                        <p class="lead font-weight-normal">Our mission is to provide you all your banking needs.</p>
+                    </div>
+                    <div class="product-device box-shadow d-none d-md-block"></div>
+                    <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
+                </div>
+                <i class="fas fa-h1 ">We are dedicated to helping you manage your money the way you want. So you don't have to worry much, okay?</i>
+                </Styles>
             </div>
         )
     }
