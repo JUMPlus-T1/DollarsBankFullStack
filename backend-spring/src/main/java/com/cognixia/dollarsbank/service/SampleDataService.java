2138800a3d07da74c99522e7a629559c6b751998
d-andres <daniel.andres.lopez91@gmail.com>
2020-09-16 09:44:56 -0500

    Removed TODO Repo

--- backend-spring/src/main/java/com/cognixia/dollarsbank/service/SampleDataService.java
+++ backend-spring/src/main/java/com/cognixia/dollarsbank/service/SampleDataService.java
@@ -4,20 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.cognixia.dollarsbank.model.Account;
-import com.cognixia.dollarsbank.model.Todo;
 import com.cognixia.dollarsbank.repository.AccountRepository;
-import com.cognixia.dollarsbank.repository.TodoRepository;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SampleDataService implements CommandLineRunner {
-	private final TodoRepository todoRepository;
 	private final AccountRepository accountRepository;
 
-	public SampleDataService(TodoRepository todoRepository, AccountRepository accountRepository) {
-		this.todoRepository = todoRepository;
+	public SampleDataService( AccountRepository accountRepository) {
 		this.accountRepository = accountRepository;
 	}
 
